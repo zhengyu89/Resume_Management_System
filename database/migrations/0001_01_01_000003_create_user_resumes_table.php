4c2abd87e5629c981b4492553bad7446ab6c6857
Benjamin Chew <ayoyoapk@gmail.com>
2025-06-18 16:36:24 +0800

    correct syntax (nullable -> nullable())

--- database/migrations/0001_01_01_000003_create_user_resumes_table.php
+++ database/migrations/0001_01_01_000003_create_user_resumes_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('user_resumes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->string('title')->nullable;
-            $table->text('about')->nullable;
+            $table->string('title')->nullable();
+            $table->text('about')->nullable();
             // Contact
             $table->string('phone_number')->nullable();
             $table->string('address')->nullable();
@@ -33,7 +33,7 @@ return new class extends Migration
             $table->foreignId('study_field_id')->constrained('study_fields')->onDelete('restrict');
             $table->string('school_name');
             $table->date('date_start');
-            $table->date('date_end')->nullable;
+            $table->date('date_end')->nullable();
             $table->double('gpa');
             $table->timestamps();
         });
@@ -43,9 +43,9 @@ return new class extends Migration
             $table->foreignId('resume_id')->constrained('user_resumes')->onDelete('cascade');
             $table->string('company_name');
             $table->string('position');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->date('date_start');
-            $table->date('date_end')->nullable;
+            $table->date('date_end')->nullable();
             $table->timestamps();
         });
 
@@ -75,12 +75,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_resumes');
         Schema::dropIfExists('user_educations');
         Schema::dropIfExists('study_fields');
         Schema::dropIfExists('user_work_experiences');
         Schema::dropIfExists('user_languages');
         Schema::dropIfExists('languages');
         Schema::dropIfExists('user_documents');
+        Schema::dropIfExists('user_resumes');
     }
 };
